725a73b8e8800a952336b3fd5db924795ed5afd1
Viba Mohan <happymorning552@gmail.com>
2026-01-22 17:14:17 -0800

    Overcomplicate lab
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>

--- Labs/ContactLab/Runner.java
+++ Labs/ContactLab/Runner.java
@@ -1,6 +1,5 @@
 
 public class Runner {
-
     public static void main(String[] args) {
         Screen screen = new Screen();
         screen.setName("Contact Interface");
--- Labs/ContactLab/Screen.java
+++ Labs/ContactLab/Screen.java
@@ -1,7 +1,6 @@
-
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Arrays;
+import java.util.Comparator;
 import javax.swing.*;
 
 public class Screen extends JFrame implements ActionListener {
@@ -42,6 +41,11 @@ public class Screen extends JFrame implements ActionListener {
         firstField = new JTextField(8);
         lastField = new JTextField(8);
         emailField = new JTextField(15);
+
+        addPlaceholder(searchField, "Search...");
+        addPlaceholder(firstField, "First");
+        addPlaceholder(lastField, "Last");
+        addPlaceholder(emailField, "Email");
 
         firstBtn = new JButton("First");
         lastBtn = new JButton("Last");
@@ -98,10 +102,31 @@ public class Screen extends JFrame implements ActionListener {
         setVisible(true);
     }
 
+    private void addPlaceholder(JTextField field, String text) {
+        field.setForeground(Color.GRAY);
+        field.setText(text);
+
+        field.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent e) {
+                if (field.getText().equals(text)) {
+                    field.setText("");
+                    field.setForeground(Color.BLACK);
+                }
+            }
+
+            public void focusLost(FocusEvent e) {
+                if (field.getText().isEmpty()) {
+                    field.setForeground(Color.GRAY);
+                    field.setText(text);
+                }
+            }
+        });
+    }
+
     private void updateDisplay() {
         display.setText("");
         for (int i = 0; i < count; i++) {
-            display.append(myList[i].toString() + "\n");
+            display.append(myList[i] + "\n");
         }
     }
 
@@ -128,49 +153,64 @@ public class Screen extends JFrame implements ActionListener {
     }
 
     private void sortBy(String type) {
-        Arrays.sort(myList, 0, count, (a, b) -> {
-            if (type.equals("first")) {
-                return a.getFirstName().compareTo(b.getFirstName());
-            }
-            if (type.equals("last")) {
-                return a.getLastName().compareTo(b.getLastName());
-            }
+        mergeSort(myList, 0, count - 1, (a, b) -> {
+            if (type.equals("first")) return a.getFirstName().compareTo(b.getFirstName());
+            if (type.equals("last")) return a.getLastName().compareTo(b.getLastName());
             return a.getUsername().compareTo(b.getUsername());
         });
         updateDisplay();
     }
 
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == firstBtn) {
-            search("first");
+    private static void mergeSort(Contact[] contacts, int l, int r, Comparator<Contact> comparator) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            mergeSort(contacts, l, m, comparator);
+            mergeSort(contacts, m + 1, r, comparator);
+            merge(contacts, comparator, l, m, r);
         }
-        if (e.getSource() == lastBtn) {
-            search("last");
-        }
-        if (e.getSource() == userBtn) {
-            search("user");
-        }
-        if (e.getSource() == domainBtn) {
-            search("domain");
-        }
-        if (e.getSource() == extBtn) {
-            search("ext");
+    }
+
+    static void merge(Contact[] arr, Comparator<Contact> comparator, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        Contact[] L = new Contact[n1];
+        Contact[] R = new Contact[n2];
+
+        for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+        for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (comparator.compare(L[i], R[j]) <= 0) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
         }
 
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == firstBtn) search("first");
+        if (e.getSource() == lastBtn) search("last");
+        if (e.getSource() == userBtn) search("user");
+        if (e.getSource() == domainBtn) search("domain");
+        if (e.getSource() == extBtn) search("ext");
+
         if (e.getSource() == addBtn && count < 10) {
-            myList[count] = new Contact(firstField.getText(), lastField.getText(), emailField.getText());
-            count++;
+            myList[count++] = new Contact(
+                    firstField.getText(),
+                    lastField.getText(),
+                    emailField.getText()
+            );
             updateDisplay();
         }
 
-        if (e.getSource() == sortFirstBtn) {
-            sortBy("first");
-        }
-        if (e.getSource() == sortLastBtn) {
-            sortBy("last");
-        }
-        if (e.getSource() == sortUserBtn) {
-            sortBy("user");
-        }
+        if (e.getSource() == sortFirstBtn) sortBy("first");
+        if (e.getSource() == sortLastBtn) sortBy("last");
+        if (e.getSource() == sortUserBtn) sortBy("user");
     }
 }
